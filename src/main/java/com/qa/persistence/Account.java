cc2dfd16a0c4d7604de94af182132d906c7d3901
oli <oli.loades@gmail.com>
2018-07-12 12:35:20 +0100

    added account entity

---
+++ src/main/java/com/qa/persistence/Account.java
@@ -0,0 +1,73 @@
+package com.qa.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Account {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(length = 50)
+	private String firstName;
+	@Column(length = 50)
+	private String surname;
+	@Column(length = 4)
+	private String accNo;
+
+	@OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Transaction> transactions;
+
+	public Account(String firstName, String surname, String accNo) {
+		this.firstName = firstName;
+		this.surname = surname;
+		this.accNo = accNo;
+		this.transactions = new ArrayList<Transaction>();
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public void addTransaction(Transaction transaction) {
+		transactions.add(transaction);
+		transaction.setAccount(this);
+	}
+
+}
---
+++ src/main/java/com/qa/persistence/Transaction.java
@@ -0,0 +1,51 @@
+package com.qa.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "account_id")
+	private Account account;
+	
+	public Transaction(String name, Account account) {
+		this.name = name;
+		this.account = account;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+
+}
